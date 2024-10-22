5c656271631b73e9421c52a777efacef8427655d
MaZen <MazenAnter>
2024-10-22 17:48:05 +0300

    init folder

--- lib/core/utiles/app_router.dart
+++ lib/core/utiles/app_router.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:meal_recommendations_a2/features/splash_boarding/splash_screen.dart';
+import 'package:meal_recommendations_a2/features/auth/register/presentation/views/register_view.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const SplashScreen();
-      },
-    ),
-  ],
-);
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const RegisterView();
+        },
+      ),
+    ],
+  );
 }
---
+++ lib/features/auth/register/presentation/views/widgets/register_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class RegisterViewBody extends StatelessWidget {
+  const RegisterViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
