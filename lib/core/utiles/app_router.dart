c2b339a42c39d5ef9d86f93d507a901ec61cc285
mohammed <mohammed.shhab.addin@gmail.com>
2024-10-27 13:55:12 +0300

    ..

--- lib/core/utiles/app_router.dart
+++ lib/core/utiles/app_router.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
-<<<<<<< HEAD
-import 'package:meal_recommendations_a2/features/auth/otp/presentation/otp_verification_screen.dart';
-import 'package:meal_recommendations_a2/features/splash_boarding/splash_screen.dart';
-=======
+
 import 'package:meal_recommendations_a2/features/auth/register/presentation/views/register_view.dart';
 
 import '../../features/auth/login/persentation/view/sign_in/login_view.dart';
 import '../../features/auth/otp/presentation/otp_verification_screen.dart';
 import '../../features/onboarding/onboarding.dart';
 import '../../features/splash_boarding/splash_screen.dart';
->>>>>>> 1b4938ddf4d049f27994fd84898070be5699f7d3
-
-import '../../features/auth/otp/presentation/cubit/otp_cubit.dart';
 
 class AppRouter {
   static const String kSplashScreen = '/';
@@ -26,8 +18,7 @@ class AppRouter {
   static const String kOnBoardingScreen = '/onboarding';
 
   static final GoRouter router = GoRouter(
-
-initialLocation: AppRouter.kSplashScreen,
+    initialLocation: AppRouter.kSplashScreen,
     routes: <RouteBase>[
       GoRoute(
         path: AppRouter.kSplashScreen,
@@ -35,32 +26,18 @@ initialLocation: AppRouter.kSplashScreen,
           return const SplashScreen();
         },
       ),
-<<<<<<< HEAD
-      GoRoute(
-        path: kOtpScreen,
-        builder: (context, state) {
-          final phoneNumber = state.uri.queryParameters['phoneNumber'] ?? '';
-          final otpCubit = GetIt.I<OTPCubit>()..sendOTP(phoneNumber);
-
-          return BlocProvider.value(
-            value: otpCubit,
-            child: OtpVerificationScreen(phoneNumber: phoneNumber),
-          );
-=======
-
       GoRoute(
         path: AppRouter.kOnBoardingScreen,
         builder: (BuildContext context, GoRouterState state) {
           return const Onboarding();
-
         },
       ),
-       GoRoute(
-      path: AppRouter.kLoginScreen,
-      builder: (BuildContext context, GoRouterState state) {
-        return const loginView();
-      },
-    ),
+      GoRoute(
+        path: AppRouter.kLoginScreen,
+        builder: (BuildContext context, GoRouterState state) {
+          return const loginView();
+        },
+      ),
       GoRoute(
         path: AppRouter.kRegisterScreen,
         builder: (BuildContext context, GoRouterState state) {
@@ -70,12 +47,11 @@ initialLocation: AppRouter.kSplashScreen,
       GoRoute(
         path: AppRouter.kOtpScreen,
         builder: (BuildContext context, GoRouterState state) {
-          return  const OtpVerificationScreen(phoneNumber: '+201151827070',);
->>>>>>> 1b4938ddf4d049f27994fd84898070be5699f7d3
+          return const OtpVerificationScreen(
+            phoneNumber: '+201151827070',
+          );
         },
       ),
     ],
   );
-
-
 }
