1ea79b55fb9ad6e642171a31e9b9b7eb14c1cf79
mohammed <mohammed.shhab.addin@gmail.com>
2024-10-27 13:38:28 +0300

    ..

--- lib/core/utiles/app_router.dart
+++ lib/core/utiles/app_router.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:meal_recommendations_a2/features/auth/otp/presentation/otp_verification_screen.dart';
 import 'package:meal_recommendations_a2/features/splash_boarding/splash_screen.dart';
+
+import '../../features/auth/otp/presentation/cubit/otp_cubit.dart';
 
 class AppRouter {
   static const String kSplashScreen = '/';
@@ -17,6 +22,18 @@ class AppRouter {
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return const SplashScreen();
+        },
+      ),
+      GoRoute(
+        path: kOtpScreen,
+        builder: (context, state) {
+          final phoneNumber = state.uri.queryParameters['phoneNumber'] ?? '';
+          final otpCubit = GetIt.I<OTPCubit>()..sendOTP(phoneNumber);
+
+          return BlocProvider.value(
+            value: otpCubit,
+            child: OtpVerificationScreen(phoneNumber: phoneNumber),
+          );
         },
       ),
     ],
--- lib/features/auth/otp/presentation/otp_verification_screen.dart
+++ lib/features/auth/otp/presentation/otp_verification_screen.dart
@@ -14,6 +14,7 @@ class OtpVerificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // u can delete this line when us GoRoute is ready
     final otpCubit = GetIt.I<OTPCubit>()..sendOTP(phoneNumber);
     print("phoneNumber: $phoneNumber");
     return Scaffold(
