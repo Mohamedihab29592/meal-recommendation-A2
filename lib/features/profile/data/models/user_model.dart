6a85fb97e61610f477a0ab47afca19c1e76f18ca
Zeyad Ali <z01551153743@gmail.com>
2024-10-31 16:07:11 +0300

    implement firebase storage services

---
+++ lib/features/profile/data/models/user_model.dart
@@ -0,0 +1,41 @@
+class UserModel {
+  final String userName;
+  final String email;
+  final String phone;
+  final String profileURL;
+  final String uId;
+
+  UserModel({
+    required this.userName,
+    required this.email,
+    required this.phone,
+    required this.profileURL,
+    required this.uId,
+  });
+
+  static const String userNameKey = "userName";
+  static const String emailKey = "email";
+  static const String phoneKey = "mobileNumber";
+  static const String profileURLKey = "profilePic";
+  static const String uIdKey = "uId";
+
+  factory UserModel.fromJSON(dynamic data) {
+    return UserModel(
+      userName: data[userNameKey],
+      email: data[emailKey],
+      phone: data[phoneKey],
+      profileURL: data[profileURLKey],
+      uId: data[uIdKey],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      userNameKey: userName,
+      emailKey: email,
+      phoneKey: phone,
+      profileURLKey: profileURL,
+      uIdKey: uId,
+    };
+  }
+}
